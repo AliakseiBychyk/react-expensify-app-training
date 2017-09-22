59083c5dffd77bf7cd86878f65943b82b12baf3e
Aliaksei Bychyk <Aliaksei_Bychyk@epam.com>
2017-09-22 16:25:33 +0300

    spreading object 3

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -33,6 +33,11 @@ const editExpense = (id, updates) => ({
 })
 
 // SET_TEXT_FILTER
+const setTextFilter = (text = '') => ({
+  type: 'SET_TEXT_FILTER',
+  text
+})
+
 // SORT_BY_DATE
 // SORT_BY_AMOUNT
 // SET_START_DATE
@@ -70,6 +75,8 @@ const filtersReducerDefaultState = {
 
 const filtersReducer = (state = filtersReducerDefaultState, action) => {
   switch (action.type) {
+    case 'SET_TEXT_FILTER':
+      return { ...state, text: action.text }  
     default:
       return state  
   }
@@ -100,7 +107,8 @@ const expenseTwo = store.dispatch(addExpense({
 store.dispatch(removeExpense({ id: expenseOne.expense.id }))
 store.dispatch(editExpense(expenseTwo.expense.id, { amount: 500 }))
 
-
+store.dispatch(setTextFilter('rent'))
+store.dispatch(setTextFilter(''))
 
 console.log(expenseOne)
 
