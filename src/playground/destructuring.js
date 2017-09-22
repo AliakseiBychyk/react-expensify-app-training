fe94d582e41a21da75f06a27421761869118fdb1
Aliaksei Bychyk <Aliaksei_Bychyk@epam.com>
2017-09-22 11:40:09 +0300

    ES6 object destructuring

---
+++ src/playground/destructuring.js
@@ -0,0 +1,33 @@
+console.log('desctructuring')
+
+
+const person = {
+  name: 'Andrew',
+  age: 26,
+  location: {
+    city: 'Philadelphia',
+    temp: 92
+  }
+}
+
+const { name: firstName = 'Anonymous', age } = person
+
+
+console.log(`${firstName} is ${age}.`)
+
+const {city, temp: temperature } = person.location
+
+if (city && temperature) console.log(`It's ${city} in ${temperature}`)
+
+
+const book = {
+  title: 'Ego is the Enemy',
+  author: 'Ryan Holiday',
+  publisher: {
+    name: 'Penguin'
+  }
+}
+
+const {name: publisherName = 'Self-Published'} = book.publisher
+
+console.log(publisherName) // Penguin, Self-Published
