35baa0e3b32066df84008e1e1e8b9bbd50a0ced6
Aliaksei Bychyk <Aliaksei_Bychyk@epam.com>
2017-09-25 16:35:28 +0300

    setting up a date picker

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react'
+import moment from 'moment'
+import { SingleDatePicker } from 'react-dates'
+import 'react-dates/lib/css/_datepicker.css'
+
+// const date = new Date()
+const now = moment()
+console.log(now.format('MMMM Do, YYYY'))
 
 class ExpenseForm extends Component {
   state = {
     description: '',
     note: '',
-    amount: ''
+    amount: '',
+    createdAt: moment(),
+    calendarFocused: false
   }
   
   onDescriptionChange = (e) => {
@@ -19,6 +28,14 @@ class ExpenseForm extends Component {
   onAmountChange = (e) => {
     const amount = e.target.value
     amount.match(/^\d*(\.\d{0,2})?$/) && this.setState({amount})
+  }
+  
+  onDateChange = (createdAt) => {
+    this.setState({ createdAt })
+  }
+  
+  onFocusChange = ({ focused }) => {
+    this.setState({ calendarFocused: focused })
   }
 
   render() {
@@ -39,14 +56,23 @@ class ExpenseForm extends Component {
             value={this.state.amount}
             onChange={this.onAmountChange}
           />
+          <SingleDatePicker
+            date={this.state.createdAt}
+            onDateChange={this.onDateChange}
+            focused={this.state.calendarFocused}
+            onFocusChange={this.onFocusChange}
+            numberOfMonths={1}
+            isOutsideRange={() => false}
+          />
+          <textarea
+            placeholder="Add a note for your expense (optional)"
+            value={this.state.note}
+            onChange={this.onNoteChange}
+          >
+          </textarea>
+          <button>Add Expense</button>
         </form>
-        <textarea
-          placeholder="Add a note for your expense (optional)"
-          value={this.state.note}
-          onChange={this.onNoteChange}
-        >
-        </textarea>
-        <button>Add Expense</button>
+
       </div>
     )
   }
