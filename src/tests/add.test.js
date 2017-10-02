6072306382daed3d0d7def4e6271bd0498b97229
Aliaksei Bychyk <Aliaksei_Bychyk@epam.com>
2017-10-02 11:49:33 +0300

    test with expect

--- src/tests/add.test.js
+++ src/tests/add.test.js
@@ -1,8 +1,13 @@
-const add = (a, b) => a + b + 1
+const add = (a, b) => a + b
+
+const generateGreeting = (name) => `Hello ${name}`
 
 test('should add two numbers', () => {
   const result = add(3, 4)
-  if (result !== 7) {
-    throw new Error(`You added 4 and 3. The result was ${result}. Expect 7`)
-  }
+  expect(result).toBe(7)
+})
+
+test('should generate greeting', () => {
+  const result = generateGreeting('Aleks')
+  expect(result).toBe('Hello Aleks')
 })
