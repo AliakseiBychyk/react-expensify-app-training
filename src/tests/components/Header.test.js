7cd1a77f085f20e1386d6f1e0c96aeb6d23b56b7
Aliaksei Bychyk <Aliaksei_Bychyk@epam.com>
2017-10-03 16:05:34 +0300

    snapshot serializer

--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -4,6 +4,8 @@ import Header from '../../components/Header'
 
 test('should render Header correctly', () => {
   const wrapper = shallow(<Header />)
-  expect(wrapper.find('h1').length).toBe(1)
-  expect(wrapper.find('h1').text()).toBe('Expensify')
+  expect(wrapper).toMatchSnapshot()
+
+  // expect(wrapper.find('h1').length).toBe(1)
+  // expect(wrapper.find('h1').text()).toBe('Expensify')
 })
