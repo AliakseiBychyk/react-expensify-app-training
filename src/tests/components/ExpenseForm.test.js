5e247fcb66feeb33157020c07ea422b82413998c
Aliaksei Bychyk <Aliaksei_Bychyk@epam.com>
2017-10-05 11:39:29 +0300

    test spies 1

--- src/tests/components/ExpenseForm.test.js
+++ src/tests/components/ExpenseForm.test.js
@@ -59,3 +59,9 @@ test('should not set amount if invalid input', () => {
   expect(wrapper.state('amount')).not.toBe(value)
   expect(wrapper.state('amount')).toBe('')
 })
+
+test('should call onSubmit prop for valid form submission', () => {
+  const onSubmitSpy = jest.fn()
+  onSubmitSpy()
+  expect(onSubmitSpy).toHaveBeenCalled()
+})
