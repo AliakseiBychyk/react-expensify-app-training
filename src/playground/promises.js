ef7c806121c58c404a968229f2a5e6c46fb575de
Aliaksei Bychyk <Aliaksei_Bychyk@epam.com>
2017-10-12 16:02:00 +0300

    promises 2

--- src/playground/promises.js
+++ src/playground/promises.js
@@ -1,13 +1,17 @@
 const promise = new Promise((resolve, reject) => {
   setTimeout(()=> {
-    resolve('this is my resolved data')
-  }, 1500)
+    // resolve('this is my resolved data')
+    reject('something went wrong')
+  }, 4500)
 })
 
 console.log('before')
 
 promise.then((data)=> {
-  console.log(data)
+  console.log('1', data)
+}).catch((err)=> {
+  console.log(err)
 })
+
 
 console.log('after')
