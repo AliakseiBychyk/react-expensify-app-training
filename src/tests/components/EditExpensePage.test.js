07416710f358243ea32de4588a09081cf6feeb51
Aliaksei Bychyk <Aliaksei_Bychyk@epam.com>
2017-10-26 13:55:07 +0300

    update expense from firebase

--- src/tests/components/EditExpensePage.test.js
+++ src/tests/components/EditExpensePage.test.js
@@ -3,18 +3,18 @@ import { shallow } from 'enzyme'
 import { EditExpensePage } from '../../components/EditExpensePage'
 import expenses from '../fixtures/expenses'
 
-let editExpense
+let startEditExpense
 let startRemoveExpense
 let history 
 let wrapper
 
 beforeEach(() => {
-  editExpense = jest.fn()
+  startEditExpense = jest.fn()
   startRemoveExpense = jest.fn()
   history = { push: jest.fn() }
   wrapper = shallow(
     <EditExpensePage
-      editExpense={editExpense}
+      startEditExpense={startEditExpense}
       startRemoveExpense={startRemoveExpense}
       history={history}
       expense={expenses[1]}
@@ -25,10 +25,10 @@ test('should render EditExpensePage correctly', () => {
   expect(wrapper).toMatchSnapshot()
 })
 
-test('should handle editExpense', () => {
+test('should handle startEditExpense', () => {
   wrapper.find('ExpenseForm').prop('onSubmit')(expenses[1])
   expect(history.push).toHaveBeenLastCalledWith('/')
-  expect(editExpense).toBeCalledWith(expenses[1].id, expenses[1])
+  expect(startEditExpense).toBeCalledWith(expenses[1].id, expenses[1])
 })
 
 test('should handle startRemoveExpense', () => {
