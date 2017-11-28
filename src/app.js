e034645e1f0a7c1c9309c3c6a624a338809e69d5
Aliaksei Bychyk <Aliaksei_Bychyk@epam.com>
2017-11-28 18:01:09 +0300

    Login and Logout

--- src/app.js
+++ src/app.js
@@ -9,25 +9,17 @@ import getVisibleExpenses from './selectors/expenses'
 import 'normalize.css/normalize.css'
 import './styles/styles.scss'
 import 'react-dates/lib/css/_datepicker.css'
-import './firebase/firebase'
+import { firebase } from './firebase/firebase'
 
 const store = configureStore()
 
-// store.dispatch(addExpense({ description: 'Water bill', amount: 4500, createdAt: 200000  }))
-// store.dispatch(addExpense({ description: 'Gas bill', createdAt: 90450000000 }))
-// store.dispatch(addExpense({ description: 'Rent', amount: 125500, createdAt: -300094599 }))
-
-// console.log(store.getState())
-
 const state = store.getState()
 const visibleExpenses = getVisibleExpenses(state.expenses, state.filters)
-// console.log(visibleExpenses)
 
 const jsx = (
   <Provider store={store} >
     <AppRouter />
   </Provider>
-  
 )
 
 ReactDOM.render(<p>Loading...</p>, document.getElementById('app'))
@@ -36,4 +28,10 @@ store.dispatch(startSetExpenses()).then(()=> {
   ReactDOM.render(jsx, document.getElementById('app'))
 })
 
-
+firebase.auth().onAuthStateChanged((user) => {
+  if (user) {
+    console.log('log in')
+  } else {
+    console.log('log out')
+  }
+})
