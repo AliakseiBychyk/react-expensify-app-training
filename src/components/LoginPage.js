1fcf6c39e94ed018bae38b16104f9e60ad695af4
Aliaksei Bychyk <Aliaksei_Bychyk@epam.com>
2017-12-21 17:26:19 +0300

    styling buttons

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -5,8 +5,13 @@ import { startLogin } from '../actions/auth'
 export class LoginPage extends Component {
   render() {
     return (
-      <div>
-        <button onClick={this.props.startLogin} >Login</button>
+      <div className="box-layout">
+        <div className="box-layout__box">
+          <h1 className="box-layout__title">Expensify App</h1>
+          <p>It's time to get your expenses under control</p>  
+          <button className="button" onClick={this.props.startLogin} >Login with Google</button>
+        </div>  
+        
       </div>
     )
   }
