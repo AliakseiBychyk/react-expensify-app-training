2bc2a2809b4bf37d0cfc5c462d57015e74e840d7
Aliaksei Bychyk <Aliaksei_Bychyk@epam.com>
2018-01-23 18:34:43 +0300

    styling expense form

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -15,15 +15,21 @@ export class EditExpensePage extends Component {
   render() {
     return (
       <div>
-        Editing the expense with id of {this.props.id}
-  
-        <ExpenseForm
-          expense={this.props.expense}  
-          onSubmit={this.onSubmit}
-        />
-        <button
-          onClick={this.onRemove}
-        >Remove</button>
+        <div className="page-header">
+          <div className="content-container">
+            <h1 className="page-header__title">Edit Expense</h1>
+          </div>
+        </div>  
+        <div className="content-container" >
+          <ExpenseForm
+            expense={this.props.expense}  
+            onSubmit={this.onSubmit}
+          />
+          <button
+            className="button button--secondary"  
+            onClick={this.onRemove}
+          >Remove Expense</button>
+        </div>  
       </div>  
     )
   }
